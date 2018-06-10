7bc32b78a0c9f583d64cb09ced68ef557035c289
itao <hi@taoian.com>
2018-06-10 10:12:11 -0400

    WIP get score board with every score entry

--- slack/app/commands/score.js
+++ slack/app/commands/score.js
@@ -1,13 +1,8 @@
 import { apiRequestUtil, requestUtil } from "../request";
 import { chatUtil } from "../client/chat";
+import { getScoreBoard } from "../templates/getScores";
 
-export const score = async (
-  channelId,
-  teamId,
-  responseUrl,
-  userId,
-  deal = "Unnamed deal"
-) => {
+export const score = async (channelId, teamId, responseUrl, userId, deal) => {
   // Check for existing challenge
   let challenges;
   try {
@@ -27,22 +22,32 @@ export const score = async (
   }
   const challenge = challenges[0];
 
+  // Check if a deal name was entered
+  let dealName = "Unnamed deal";
+  if (deal.length > 0) {
+    dealname = deal;
+  }
+
   // Assume only one metric per challenge for now, record it immediately
   try {
     const createdActivity = await apiRequestUtil.createActivity(
       challenge.challengeId,
       teamId,
       userId,
-      deal
+      dealName
     );
-    console.log(userId);
-    console.log(createdActivity);
-    return chatUtil.postMessage({
+
+    // Format Slack message
+    console.log(channelId);
+    const message = {
       channel: channelId,
       text: `<@${userId}> just scored a \`${challenge.metric}\` with \`${
         createdActivity.deal
-      }\`!`
-    });
+      }\`!`,
+      attachments: await getScoreBoard(challenge)
+    };
+
+    return chatUtil.postMessage(message);
   } catch (error) {
     console.log(error);
     return requestUtil.post(responseUrl, { text: error });
