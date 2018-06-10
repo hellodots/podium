77f6027d336a68789ee155db8e00bbc068a80b72
itao <hi@taoian.com>
2018-06-10 01:30:20 -0400

    enter score

---
+++ slack/app/actions/scoreSubmission.js
@@ -0,0 +1,10 @@
+import { chatUtil } from "../client/chat";
+import { apiRequestUtil } from "../request";
+import { getScoreBoard } from "../templates/getScores";
+
+export const scoreSubmission = async (
+  channelId,
+  teamId,
+  userId,
+  submission
+) => {};
