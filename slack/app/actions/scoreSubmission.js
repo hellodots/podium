329af6ef7b2e511c561ff4118243c42046ff0b89
itao <hi@taoian.com>
2018-06-11 13:23:48 -0400

    remove unused leaderboard code for now

--- slack/app/actions/scoreSubmission.js
+++ slack/app/actions/scoreSubmission.js
@@ -7,32 +7,4 @@ export const scoreSubmission = async (
   teamId,
   userId,
   submission
-) => {
-  // Todo: save activity to database
-  try {
-    // const challenge = await apiRequestUtil.createChallenge(
-    //   channelId,
-    //   teamId,
-    //   userId,
-    //   submission.metric,
-    //   submission.duration
-    // );
-
-    const scoreAttachment = getScoreBoard(1);
-    const message = {
-      channel: channelId,
-      text: `<@${userId}> just scored a \`Deal created\` for 1 point with \`Acme Corp\`!`,
-      attachments: scoreAttachment
-    };
-
-    return chatUtil.postMessage(message);
-  } catch (error) {
-    // TODO: Update error message
-    // Message the error to the user
-    return chatUtil.postEphemeral({
-      channel: channelId,
-      user: userId,
-      text: error
-    });
-  }
-};
+) => {};
